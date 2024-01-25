062a8530b251ff2e897d190ee9139098e1b45476
Romain-GRADELET <romain.gradelet73@outlook.fr>
2024-01-25 16:43:52 +0100

    try catch

--- poo-v2.cpp
+++ poo-v2.cpp
@@ -3,63 +3,32 @@
 
 using namespace std;    
 
-class Perso
-{
-public:
-    int xp;
-    int power;
-
-    // *** Constructeur ***
-    Perso(int xp2 = 0) 
-    {
-        xp = xp2;
-        power = 10;
-    }
-    // ********************
-
-private:
-    int gold;
-
-
-protected:
-
-
-};
-
-class Archer : public Perso
-{
-public:
-    int dist;
-
-private:
-
-};
-
-class Guerrier : public Perso
-{
-public:
-    int bouclier;
-
-private:
-
-};
 
 int main()
 {
-    // nom, xp, or, for, def
+    int demo[] = { 10, 20, 30, 40, 50 };
+    int nb{};
 
-    Archer personnage1;
-    Guerrier personnage2;
-    personnage2.xp = 5;
+    cout << "Saisir un nombre : " << endl;
+    cin >> nb;
 
-    cout << "xp perso 1 : " << personnage1.xp << endl;
-    cout << "xp perso 1 : " << personnage2.xp << endl;
+    if (nb < 5)
+    {
+        cout << "demo[nb] = " << demo[nb] << endl;
+    }
+    else
+    {
+        try
+        {
+            throw - 1;
+        }
+        catch (int error)
+        {
+            cout << "Probleme. Code erreur : " << error << endl;
+        }
+    }
 
-    personnage1.dist = 40;
-    personnage2.bouclier = 100;
-
-    cout << "dist perso 1 : " << personnage1.dist << endl;
-    cout << "bouclier perso 1 : " << personnage2.bouclier << endl;
+    cout << "Fin du programme" << endl;
 
     return 0;
 }
